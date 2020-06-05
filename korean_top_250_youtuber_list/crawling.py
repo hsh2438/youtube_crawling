6850c87205c3b5fece538444d918d91ae5fe0cf6
seokhyun <hsh2438@naver.com>
2020-06-05 15:04:08 +0900

    Create crawling.py

---
+++ korean_top_250_youtuber_list/crawling.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+driver = webdriver.Chrome('chromedriver')
+
+driver.get('https://kr.noxinfluencer.com/youtube-channel-rank/top-250-all-all-youtuber-sorted-by-subs-weekly')
+
+def get_items():
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    driver.implicitly_wait(3)
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    table = soup.find('table', attrs={'class':'kol-table'})
+    items = table.find_all('tr', attrs={'class':'item'})
+    return items
+
+while True:
+    items = get_items()
+    if len(items) == 250:
+        break
+
+# save
+with open('korean_top_250_youtuber.tsv', 'w', encoding='utf-8') as fw:
+    for item in items:
+        name = item.find('td', attrs={'class':'profile'}).find('span')['title']
+        address = item.find('td', attrs={'class':'profile'}).find('a', href=True)['href']
+        category = item.find('td', attrs={'class':'text category'})['title']
+        subscriber = item.find('td', attrs={'class':'text followerNum with-num'}).find('span', attrs={'class':'num'}).text.strip()
+        fw.write('\t'.join([name, category, subscriber, address]) + '\n')
+
+print('done')
